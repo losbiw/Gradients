5c58ddd9cc1b9c4822bc7f072da50431081ff788
Vladislav Skorinov <57506835+losbiw@users.noreply.github.com>
2020-02-29 11:42:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = function(){
-    /*let gradients = [["#4e54c8", "#8f94fb"], ["#FDC830", "#F37335"], ["#FF416C", "#FF4B2B"], ["#00b09b", "#96c93d"], ["#007991", "#78ffd6"], ["#f953c6", "#b91d73"],
-    ["#f12711", "#f5af19"], ["#DAD299", "#B0DAB9"], ["#7F00FF", "#E100FF"], ["#e1eec3", "#f05053"]];*/
-    import gradients from 'https://cdn.jsdelivr.net/gh/losbiw/Gradients/gradients.js';
+    let gradients = [["#4e54c8", "#8f94fb"], ["#FDC830", "#F37335"], ["#FF416C", "#FF4B2B"], ["#00b09b", "#96c93d"], ["#007991", "#78ffd6"], ["#f953c6", "#b91d73"],
+    ["#f12711", "#f5af19"], ["#DAD299", "#B0DAB9"], ["#7F00FF", "#E100FF"], ["#e1eec3", "#f05053"]];
     //Directions for gradientss
     let positions = ['to right', 'to left', 'to top', 'to bottom', 'to right top', 'to right bottom', 'to left top', 'to left bottom'];
 
